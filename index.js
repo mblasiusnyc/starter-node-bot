95ded5a173349d10ebab6a0b109af6c50890fd51
Michael Blasius <michael.blasius@readytalk.com>
2016-05-31 11:53:11 -0600

    Another sample chagne

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ controller.on('bot_channel_join', function (bot, message) {
 })
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Get off my Lawn!')
+  bot.reply(message, 'Hello')
 })
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+  bot.reply(message, 'Get off my Lawn!')
   bot.reply(message, 'It\'s nice to talk to you directly.')
 })
 
