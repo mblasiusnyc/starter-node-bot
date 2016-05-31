5ba227834cc3b090476cbf418598384d36530ac4
Michael Blasius <michael.blasius@readytalk.com>
2016-05-31 13:55:17 -0600

    Whatever2

--- index.js
+++ index.js
@@ -27,11 +27,7 @@ if (token) {
 }
 
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
-
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello')
+  bot.reply(message, "TimeToTalkBot has joined the channel.")
 })
 
 controller.hears(['/(.*)time to talk(.*)/g]', ['message_received'], function (bot, message) {
@@ -53,53 +49,37 @@ controller.hears(['/(.*)time to talk(.*)/g]', ['message_received'], function (bo
   })
 })
 
-// controller.hears(['time to talk'], ['direct_message'], function (bot, message) {
-//   bot.startConversation(message,function(err,convo) {
-//     convo.ask('How are you?',function(response,convo) {
-
-//       convo.say('Cool, you said: ' + response.text);
-//       convo.next();
-
-//     });
-
-//   })
+// controller.hears('.*', ['mention'], function (bot, message) {
+//   bot.reply(message, 'You really do care about me. :heart:')
 // })
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
+// controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+//   var help = 'I will respond to the following messages: \n' +
+//       '`bot hi` for a simple message.\n' +
+//       '`bot attachment` to see a Slack attachment message.\n' +
+//       '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+//       '`bot help` to see this again.'
+//   bot.reply(message, help)
+// })
 
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
+// controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+//   var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+//   var attachments = [{
+//     fallback: text,
+//     pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+//     title: 'Host, deploy and share your bot in seconds.',
+//     image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//     title_link: 'https://beepboophq.com/',
+//     text: text,
+//     color: '#7CD197'
+//   }]
 
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-})
+//   bot.reply(message, {
+//     attachments: attachments
+//   }, function (err, resp) {
+//     console.log(err, resp)
+//   })
+// })
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
