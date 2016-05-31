c805a6462f6cb90a668b1b40534b0740bbae3185
Michael Blasius <michael.blasius@readytalk.com>
2016-05-31 16:44:59 -0600

    added date reminder

--- index.js
+++ index.js
@@ -59,15 +59,15 @@ controller.hears('(@.*) time to talk about (.*)\?', ['direct_message', 'message_
             convo.next();
             if(recipientResponse == 'yes') {
               convo.say('Great! I will remind you when its time to talk with @mblasius about '+subject+'.');
-              // var hour = suggestedTime.split(':')[0];
-              // var minute = suggestedTime.split(':')[1].substring(0,2);
-              // // var ampm = suggestedTime.match(/(AM|PM)/)
-              // var today = newDate();
-              // var date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hour, minute, 1);
-              // bot.reply(message, 'date: '+date)
-              // var reminder = schedule.scheduleJob(date, function(){
-              //   bot.reply(message, 'It is now time to talk about '+subject+'.');
-              // });
+              var hour = suggestedTime.split(':')[0];
+              var minute = suggestedTime.split(':')[1].substring(0,2);
+              // var ampm = suggestedTime.match(/(AM|PM)/)
+              var today = new Date();
+              var date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hour, minute, 1);
+              bot.reply(message, 'date: '+date)
+              var reminder = schedule.scheduleJob(date, function(){
+                bot.reply(message, 'It is now time to talk about '+subject+'.');
+              });
             } else {
               convo.say('Ok.')
             }
