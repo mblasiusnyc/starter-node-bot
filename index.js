d07dc651ad1b54d4ec885f524ca21d038b67b0d3
Michael Blasius <michael.blasius@readytalk.com>
2016-06-01 17:36:53 -0600

    added in request libraryt

--- index.js
+++ index.js
@@ -42,57 +42,64 @@ if (token) {
 // })
 
 controller.hears('(@.*) time to talk about (.*)\?', ['direct_message', 'message_received', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'I heard a message.')
+  // bot.reply(message, 'I heard a message.')
   var recipient = message.match[0].split(':')[0].substring(1, message.match[0].split(':')[0].length-1);
-  var subject = message.match[2].replace('?', '');
-  bot.reply(message, 'recipient: ' +recipient)
-  bot.reply(message, 'subject: ' +subject)
-  request.post({url:'https://slack.com/api/users.info', 
-    form: {
-      token: 'xoxp-47069036593-47053403748-47196571601-d72b35c5f7',
-      user: recipient
-    }
-  }, function(err,httpResponse,body){
-    recipient = JSON.parse(body).user;
-    bot.reply(message, 'recipient: ' +recipient);
-  })
-
-  bot.startConversation(message, function(err, convo){
-    convo.ask('You mentioned that you would like to talk to '+recipient.name+' about ' +subject+ '. Would you like to set up a reminder to do so?', function(response, convo) {
-      if(response.text == 'yes') {
-        convo.next();
-        convo.ask('Great! When would you like to talk to '+recipient.name+'?', function(response, convo) {
-          convo.next();
-          var suggestedTime = response.text;
-          // convo.say('You said you want to meet at ' + suggestedTime)
-          convo.ask('@'+recipient.name+': Are you available to meet at '+suggestedTime+' to discuss '+subject+'?', function(response, convo) {
-            var recipientResponse = response.text;
-            convo.next();
-            if(recipientResponse == 'yes') {
-              convo.say('Great! I will remind you when its time to talk with @'+recipient.name+' about '+subject+'.');
-              var hour = Number(suggestedTime.split(':')[0])+6;
-              var minute = Number(suggestedTime.split(':')[1].substring(0,2));
-              var ampm = suggestedTime.match(/PM/)
-              if(ampm) hour = Number(hour)+12;
-              var today = new Date();
-              var date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hour, minute, 1);
-              // bot.reply(message, 'hour: '+hour+ ' minute: '+minute+ ' ampm: '+ampm)
-              // bot.reply(message, 'date: '+date)
-              var reminder = schedule.scheduleJob(date, function(){
-                bot.reply(message, 'It is now time to talk about '+subject+'.');
-              });
-            } else {
-              convo.say('Ok. I won\'t remind you');
-            }
-          })
-        })
-      } else {
-        convo.say('Alrighty then.')
-        convo.next()
-      }
-    })
-  })
+  // var subject = message.match[2].replace('?', '');
+  // bot.reply(message, 'recipient: ' +recipient)
+  // bot.reply(message, 'subject: ' +subject)
+  // request.post({url:'https://slack.com/api/users.info', 
+  //   form: {
+  //     token: 'xoxp-47069036593-47053403748-47196571601-d72b35c5f7',
+  //     user: recipient
+  //   }
+  // }, function(err,httpResponse,body){
+  //   recipient = JSON.parse(body).user;
+  //   bot.reply(message, 'recipient: ' +recipient);
+  // })
+bot.startPrivateConversation(message, function(err, convo) {
+  convo.say('This is private!')
 })
+
+  // bot.startConversation(message, function(err, convo){
+  //   convo.ask('You mentioned that you would like to talk to '+recipient.name+' about ' +subject+ '. Would you like to set up a reminder to do so?', function(response, convo) {
+  //     if(response.text == 'yes') {
+  //       convo.next();
+  //       convo.ask('Great! When would you like to talk to '+recipient.name+'?', function(response, convo) {
+  //         convo.next();
+  //         var suggestedTime = response.text;
+  //         // convo.say('You said you want to meet at ' + suggestedTime)
+  //         convo.ask('@'+recipient.name+': Are you available to meet at '+suggestedTime+' to discuss '+subject+'?', function(response, convo) {
+  //           var recipientResponse = response.text;
+  //           convo.next();
+  //           if(recipientResponse == 'yes') {
+  //             convo.say('Great! I will remind you when its time to talk with @'+recipient.name+' about '+subject+'.');
+  //             var hour = Number(suggestedTime.split(':')[0])+6;
+  //             var minute = Number(suggestedTime.split(':')[1].substring(0,2));
+  //             var ampm = suggestedTime.match(/PM/)
+  //             if(ampm) hour = Number(hour)+12;
+  //             var today = new Date();
+  //             var date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hour, minute, 1);
+  //             // bot.reply(message, 'hour: '+hour+ ' minute: '+minute+ ' ampm: '+ampm)
+  //             // bot.reply(message, 'date: '+date)
+  //             var reminder = schedule.scheduleJob(date, function(){
+  //               bot.reply(message, 'It is now time to talk about '+subject+'.');
+  //             });
+  //           } else {
+  //             convo.say('Ok. I won\'t remind you');
+  //           }
+  //         })
+  //       })
+  //     } else {
+  //       convo.say('Alrighty then.')
+  //       convo.next()
+  //     }
+  //   })
+  // })
+})
+
+
+
+
 
 // controller.hears(['time to talk'], ['direct_message'], function (bot, message) {
 //   // start a conversation to handle this response.
